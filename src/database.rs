436d063ca474923fa7f2264975a9739ad441c64c
erer1243 <erer1243@users.noreply.github.com>
2023-12-10 13:42:20 -0500

    checkpoint

--- src/database.rs
+++ src/database.rs
@@ -4,6 +4,7 @@ use std::{
     io::{BufReader, BufWriter},
     path::{Path, PathBuf},
     rc::Rc,
+    time::{Duration, SystemTime},
 };
 
 use blake3::Hash;
@@ -13,29 +14,27 @@ use slotmap::SlotMap;
 
 slotmap::new_key_type! { struct BackupFileKey; }
 
+#[derive(Default, Debug)]
 pub struct BackupView {
     directories: BTreeSet<PathBuf>,
     symlinks: BTreeMap<PathBuf, PathBuf>,
 
     files: SlotMap<BackupFileKey, BackupFile>,
-    files_by_path: HashMap<Rc<Path>, BackupFileKey>,
     files_by_inode: HashMap<u64, BackupFileKey>,
 }
 
 impl BackupView {
-    fn expand(mini: MinimizedBackup) -> Self {
+    fn expand(mini: MinimizedBackup, db: &Database) -> Self {
         let mut files = SlotMap::with_capacity_and_key(mini.files.len());
-        let mut files_by_path = HashMap::with_capacity(mini.files.len());
         let mut files_by_inode = HashMap::with_capacity(mini.files.len());
 
         for (path, file) in mini.files {
-            let path: Rc<Path> = Rc::from(path);
             let key = files.insert(BackupFile {
                 path: path.clone(),
                 hash: file.hash,
                 source_inode: file.source_inode,
+                data_block_mtime: db.get_data_block_mtime(file.hash),
             });
-            files_by_path.insert(path, key);
             files_by_inode.insert(file.source_inode, key);
         }
 
@@ -43,15 +42,8 @@ impl BackupView {
             directories: mini.directories,
             symlinks: mini.symlinks,
             files,
-            files_by_path,
             files_by_inode,
         }
-    }
-
-    pub fn file_by_path<P: AsRef<Path>>(&self, p: P) -> Option<&BackupFile> {
-        self.files_by_path
-            .get(p.as_ref())
-            .map(|key| self.files.get(*key).unwrap())
     }
 
     pub fn file_by_inode(&self, ino: u64) -> Option<&BackupFile> {
@@ -61,20 +53,43 @@ impl BackupView {
     }
 }
 
+#[derive(Debug)]
 pub struct BackupFile {
-    pub path: Rc<Path>,
+    pub path: PathBuf,
     pub hash: Hash,
     pub source_inode: u64,
+    pub data_block_mtime: MTime,
 }
 
+#[derive(Default, Debug)]
 pub struct BackupBuilder {
+    // TODO: Use Rc<Path> as key to mini.files, and
+    // have a cache of Rc<Paths> here from the BackupView being updated
+    // so they can be reused instead of cloning PathBufs a bunch
     mini: MinimizedBackup,
 }
 
-impl BackupBuilder {}
+impl BackupBuilder {
+    pub fn new() -> Self {
+        Self::default()
+    }
 
-#[derive(Serialize, Deserialize)]
-struct Database {
+    pub fn insert_directory(&mut self, path: PathBuf) {
+        self.mini.directories.insert(path);
+    }
+
+    pub fn insert_symlink(&mut self, path: PathBuf, target: PathBuf) {
+        self.mini.symlinks.insert(path, target);
+    }
+
+    pub fn insert_file(&mut self, path: PathBuf, hash: Hash, source_inode: u64) {
+        let mbf = MinimizedBackupFile { hash, source_inode };
+        self.mini.files.insert(path, mbf);
+    }
+}
+
+#[derive(Serialize, Deserialize, Default)]
+pub struct Database {
     backups: BTreeMap<String, MinimizedBackup>,
     data_blocks: HashMap<Hash, DataBlockMetadata>,
 }
@@ -91,16 +106,22 @@ impl Database {
         Ok(())
     }
 
-    pub fn take_backup_view(&mut self, name: &str) -> Option<BackupView> {
-        self.backups.remove(name).map(BackupView::expand)
+    pub fn take_backup(&mut self, name: &str) -> Option<BackupView> {
+        self.backups
+            .remove(name)
+            .map(|mini| BackupView::expand(mini, self))
     }
 
     pub fn insert_backup_builder(&mut self, name: &str, bb: BackupBuilder) {
         self.backups.insert(name.to_owned(), bb.mini);
     }
+
+    fn get_data_block_mtime(&self, hash: Hash) -> MTime {
+        self.data_blocks.get(&hash).unwrap().mtime
+    }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Default)]
 struct MinimizedBackup {
     files: BTreeMap<PathBuf, MinimizedBackupFile>,
     directories: BTreeSet<PathBuf>,
@@ -118,8 +139,30 @@ struct DataBlockMetadata {
     mtime: MTime,
 }
 
-#[derive(Serialize, Deserialize)]
-struct MTime {
-    sec: i64,
+#[derive(Serialize, Deserialize, Copy, Clone, PartialEq, Eq, Ord, Debug)]
+pub struct MTime {
+    sec: u64,
     nano: u32,
+}
+
+impl MTime {
+    fn as_duration(self) -> Duration {
+        Duration::new(self.sec, self.nano)
+    }
+}
+
+impl PartialOrd for MTime {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.as_duration().partial_cmp(&other.as_duration())
+    }
+}
+
+impl From<SystemTime> for MTime {
+    fn from(st: SystemTime) -> Self {
+        let dur = st.duration_since(SystemTime::UNIX_EPOCH).unwrap();
+        MTime {
+            sec: dur.as_secs(),
+            nano: dur.subsec_nanos(),
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,15 @@ mod database;
 
 use blake3::{Hash, Hasher};
 use clap::Parser;
-use eyre::{ensure, eyre, Context, Result};
+use database::{BackupBuilder, BackupView, Database, MTime};
+use eyre::{bail, ensure, eyre, Context, Result};
 use serde::{Deserialize, Serialize};
 use std::{
     collections::{BTreeMap, BTreeSet, HashMap},
     env::current_dir,
     fmt::Debug,
-    fs::{read_dir, read_link, File},
-    io::{copy, BufReader, BufWriter},
+    fs::{read_dir, read_link, File, Metadata},
+    io::{self, copy, BufReader, BufWriter},
     path::{Path, PathBuf},
     process::exit,
 };
@@ -20,14 +21,22 @@ use walkdir::{DirEntry, DirEntryExt, WalkDir};
 #[derive(Parser)]
 enum Command {
     Init,
-    Backup { database: PathBuf, dir: PathBuf },
+    Backup {
+        database: PathBuf,
+        backup_name: String,
+        dir: PathBuf,
+    },
 }
 
 fn main() {
     let cmd = Command::parse();
     let res = match cmd {
         Command::Init => cmd_init(),
-        Command::Backup { database, dir } => cmd_backup(&database, "XXX", &dir),
+        Command::Backup {
+            database,
+            backup_name,
+            dir,
+        } => cmd_backup(&database, &backup_name, &dir),
     };
     if let Err(e) = res {
         eprintln!("[error] {e:#}");
@@ -45,71 +54,62 @@ fn cmd_init() -> Result<()> {
     Database::default().write("database.json")
 }
 
-fn cmd_backup(db_path: &Path, name: &str, root: &Path) -> Result<()> {
+fn cmd_backup(db_path: &Path, bkup_name: &str, root: &Path) -> Result<()> {
     let mut db = Database::load(db_path).context_2("loading database", db_path)?;
-    let old_backup = db.backups.get(name).unwrap_or_else(|| todo!());
-    let mut new_backup = Backup::default();
+    let old = db.take_backup(bkup_name).unwrap_or_default();
+    let mut new = BackupBuilder::new();
 
-    for dir_entry_res in WalkDir::new(root) {
+    // Scan dir entries
+    for dir_entry_res in WalkDir::new(root).into_iter().skip(1) {
         // TODO: Gracefully allow failure
         let dir_entry = dir_entry_res.context("dir_entry")?;
-
-        let path = dir_entry.path();
-        let stripped_path = path.strip_prefix(root).unwrap().to_owned();
-        let metadata = dir_entry.metadata().context_2("metadata", path)?;
-
-        if metadata.is_dir() {
-            new_backup.directories.insert(stripped_path);
-        } else if metadata.is_symlink() {
-            let link_target = read_link(path).context("read_link")?;
-            new_backup.symlinks.insert(stripped_path, link_target);
-        } else if metadata.is_file() {
-            let inode = dir_entry.ino();
-            let mut file = BufReader::new(File::open(path).context("open")?);
-            let mut hasher = Hasher::new();
-            copy(&mut file, &mut hasher).context("hashing file")?;
-            let hash = hasher.finalize();
-            new_backup.files.insert(stripped_path, hash);
-        } else {
-            Err(eyre!("special file")).context_debug(path)?;
-        }
+        backup_dir_entry(&mut db, &old, &mut new, root, &dir_entry)
+            .context_debug(dir_entry.path())?;
     }
+
+    println!("{old:#?}");
+    println!("########");
+    println!("{new:#?}");
 
     Ok(())
 }
 
-fn backup_dir_entry(bkup: &mut Backup, dir_entry: &DirEntry, path: &Path) -> Result<()> {
-    let metadata = dir_entry.metadata().context("metadata")?;
+fn backup_dir_entry(
+    db: &mut Database,
+    old: &BackupView,
+    new: &mut BackupBuilder,
+    root: &Path,
+    dir_entry: &DirEntry,
+) -> Result<()> {
+    let path = dir_entry.path();
+    let stripped_path = path.strip_prefix(root).unwrap().to_owned();
+    let metadata = dir_entry.metadata().context("stat")?;
 
-    if metadata.is_file() {
-        backup_file(bkup, path)
-    } else if metadata.is_dir() {
-        backup_dir(bkup, path)
+    if metadata.is_dir() {
+        new.insert_directory(stripped_path);
     } else if metadata.is_symlink() {
-        backup_symlink(bkup, path)
+        let link_target = read_link(path).context("readlink")?;
+        new.insert_symlink(stripped_path, link_target);
+    } else if metadata.is_file() {
+        let inode = dir_entry.ino();
+        let mtime: MTime = metadata.modified().context("mtime")?.into();
+        let hash = match old.file_by_inode(inode) {
+            Some(old_file) if mtime <= old_file.data_block_mtime => old_file.hash,
+            _ => hash_file(path).context("hash_file")?,
+        };
+        new.insert_file(stripped_path, hash, inode);
     } else {
-        Err(eyre!("special file"))
+        bail!("{path:?}: special file");
     }
+
+    Ok(())
 }
 
-fn backup_file(bkup: &mut Backup, path: &Path) -> Result<()> {
-    let mut file = BufReader::new(File::open(path).context("open")?);
+fn hash_file<P: AsRef<Path>>(p: P) -> io::Result<Hash> {
+    let mut file = BufReader::new(File::open(p)?);
     let mut hasher = Hasher::new();
-    copy(&mut file, &mut hasher).context("hashing file")?;
-    let hash = hasher.finalize();
-    bkup.files.insert(path.into(), hash);
-    Ok(())
-}
-
-fn backup_dir(bkup: &mut Backup, path: &Path) -> Result<()> {
-    bkup.directories.insert(path.into());
-    Ok(())
-}
-
-fn backup_symlink(bkup: &mut Backup, path: &Path) -> Result<()> {
-    let link_target = read_link(path).context("read_link")?;
-    bkup.symlinks.insert(path.into(), link_target);
-    Ok(())
+    copy(&mut file, &mut hasher)?;
+    Ok(hasher.finalize())
 }
 
 trait ContextExt<T, E>: Context<T, E> + Sized {
